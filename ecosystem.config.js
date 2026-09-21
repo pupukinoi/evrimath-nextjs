eeedfa0130d4d2bac31f4716812b5eb82adcdfe5
saharatboonkhum <saharatboonkhum@gmail.com>
2026-09-21 10:34:43 +0700

    Configure static export and Cloudflare Pages direct deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,1 +1,1 @@
-module.exports = { apps : [{ name : 'evrima-web', script : 'npm', args : 'run start' }] }
+module.exports = { apps : [{ name : 'evrima-web', script : 'node_modules/next/dist/bin/next', args : 'start -p 3000' }] }
